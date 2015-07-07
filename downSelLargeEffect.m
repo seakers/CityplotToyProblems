d2f3f45368eb1bb45e46f1669f4ef85f04865f2e
SEAKLAB <seak@cornell.edu>
2015-07-06 20:09:33 -0400

    simplified downSelLargeEffect

--- downSelLargeEffect.m
+++ downSelLargeEffect.m
@@ -5,9 +5,7 @@ function [metrics]=downSelLargeEffect(archs)
 % w=[ 0.1070    0.0050    0.8170    0.0840    0.2600    0.4310    0.1820    0.1460    0.8690    0.5500;
 %     0.9620    0.7750    0.8690    0.4000    0.8000    0.9110    0.2640    0.1360    0.5800    0.1450]';
 
-w=ones(10,2);
-w=sort(w,'descend'); 
-w=w.*10.^(repmat(linspace(0,-3,10)',1,2));
+w=10.^(repmat(linspace(0,-3,10)',1,2));
 w(:,2)=flipud(w(:,2));
 w=w/w(end);
 
